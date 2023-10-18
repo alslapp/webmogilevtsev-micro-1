11e131800163a70ce4afe2ab0170a025105f3249
al.slapp <al.slapp@gmail.com>
2023-10-18 13:53:01 +0300

    ADD: all exceptions filter

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExeptionsFilter } from './filters';
 
-@Module({})
+@Module({
+	providers: [
+		{
+			provide: APP_FILTER,
+			useClass: AllExeptionsFilter,
+		}
+	],
+})
 export class SharedModule { }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from 'lib/providers';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [
+  imports: [
+    SharedModule,
     ProvidersModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
