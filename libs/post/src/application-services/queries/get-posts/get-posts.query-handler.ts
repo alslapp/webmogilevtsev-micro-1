10fe402e20cfdf71b0fb1014e0d2875286f29064
al.slapp <al.slapp@gmail.com>
2023-10-19 18:02:46 +0300

    UPD: PostFacade. Realisation of repository post adapter

--- libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
+++ libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
@@ -5,20 +5,20 @@ import { PostRepository } from '@lib/post/providers';
 import { Logger } from '@nestjs/common';
 
 @QueryHandler(GetPostsQuery)
-export class GetPostsQueryHandler implements IQueryHandler<GetPostsQuery, [[PostAggregate], number]> {
+export class GetPostsQueryHandler implements IQueryHandler<GetPostsQuery, [PostAggregate[], number]> {
 	private readonly logger = new Logger(GetPostsQueryHandler.name);
 
 	constructor(
 		private readonly postRepository: PostRepository
 	) { }
-	async execute({ pagination }: GetPostsQuery): Promise<[[PostAggregate], number]> {
+	async execute({ pagination }: GetPostsQuery): Promise<[PostAggregate[], number]> {
 		const posts = await this.postRepository
 			.findAll(pagination)
 			.catch(err => {
 				this.logger.error(err);
 				return [[], 0];
 			});
-		return posts as [[PostAggregate], number];
+		return posts as [PostAggregate[], number];
 	}
 
 }
--- libs/post/src/providers/post.repository.ts
+++ libs/post/src/providers/post.repository.ts
@@ -4,6 +4,6 @@ import { IPost, PostAggregate } from '../domain';
 export abstract class PostRepository {
 	abstract save(post: IPost): Promise<PostAggregate>;
 	abstract findOne(id: string): Promise<PostAggregate | null>;
-	abstract findAll(pagination: PaginationDto): Promise<[[PostAggregate], number]>; // так возвращает TypeORM
+	abstract findAll(pagination: PaginationDto): Promise<[PostAggregate[], number]>; // так возвращает TypeORM
 	abstract delete(id: string): Promise<boolean>;
 }
