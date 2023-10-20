8ebc1affc2bd3a6f6d6ba3045f3c025fea90d66f
al.slapp <al.slapp@gmail.com>
2023-10-20 08:52:20 +0300

    ADD: auth & current user decorator

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,7 +1,13 @@
 import { PostFacade } from '@lib/post/application-services';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto';
 
 @Controller('post')
 export class PostController {
 	constructor(private readonly postFacade: PostFacade) { }
+
+	@Post()
+	createPost(@Body() createPostDto: CreatePostDto) {
+		return this.postFacade.commands.createPost(createPostDto);
+	}
 }
