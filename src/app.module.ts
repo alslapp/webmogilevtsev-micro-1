5e6eaa0da77d7b8950a3d88f5309cbc0fd45ac18
al.slapp <al.slapp@gmail.com>
2023-10-20 08:40:30 +0300

    ADD: domains module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from 'lib/providers';
 import { ApiModule } from './api';
+import { DomainsModule } from './domains/domains.module';
 
 @Module({
   imports: [
     SharedModule,
     ProvidersModule,
     ApiModule,
+    DomainsModule,
   ],
   controllers: [],
   providers: [],
