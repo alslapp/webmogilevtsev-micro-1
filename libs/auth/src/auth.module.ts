28bafaf0d3895cac033f976253be08beee611076
al.slapp <al.slapp@gmail.com>
2023-10-24 11:18:33 +0300

    ADD: jwt strategy

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtFactory } from './configs';
 import { GUARDS } from './guards';
+import { STRATEGIES } from './strategies';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { GUARDS } from './guards';
   providers: [
     AuthService,
     ...GUARDS,
+    ...STRATEGIES,
   ],
   exports: [AuthService],
 })
