521637c3537cb5f3a6d69fb5a15c2da2dfb7e0cb
al.slapp <al.slapp@gmail.com>
2023-10-24 17:56:38 +0300

    ADD: Swagger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Posts')
+    .setDescription('CRUD API by posrs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
 
   const globalPrefix = ''; // api
 
