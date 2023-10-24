74cae30c14d21bfceb6c4f5bbd9d57fece20e5b9
al.slapp <al.slapp@gmail.com>
2023-10-24 13:06:53 +0300

    ADD: get one post by id

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,12 +1,22 @@
 import { PostFacade } from '@lib/post/application-services';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+	Param,
+	Body,
+	Controller,
+	Get,
+	Post,
+	UseGuards,
+	Logger,
+	ParseUUIDPipe,
+} from '@nestjs/common';
 import { CreatePostDto } from './dto';
-import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { CurrentUser, ICurrentUser, Public } from '@lib/auth';
 import { JwtGuard } from '@lib/auth/guards';
 
 @UseGuards(JwtGuard)
 @Controller('post')
 export class PostController {
+	private readonly logger = new Logger(PostController.name);
 	constructor(private readonly postFacade: PostFacade) { }
 
 	@Post()
@@ -16,4 +26,14 @@ export class PostController {
 			authorId: user.userId,
 		});
 	}
+
+	@Public()
+	@Get(':id')
+	getPostById(@Param('id', ParseUUIDPipe) id: string) {
+
+		this.logger.log({ id })
+
+		return this.postFacade.queries.getOnePost(id);
+	}
+
 }
