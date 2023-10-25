5ef40f6f1bfd026e33e582ae9a0e5861a7e9a2c0
al.slapp <al.slapp@gmail.com>
2023-10-25 10:45:55 +0300

    ADD: resolver Query get one post

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ControllersModule } from './controllers';
 import { AuthModule } from '@lib/auth';
+import { ResolversModule } from './resolvers';
 
 @Module({
   imports: [
     ControllersModule,
     AuthModule,
+    ResolversModule,
   ],
 })
 export class ApiModule { }
