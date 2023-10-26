777b02c67b2536fd9923a6702bc6de564e13e298
al.slapp <al.slapp@gmail.com>
2023-10-26 10:48:59 +0300

    UPD: exceptions filter

--- libs/shared/src/filters/all-exeptions.filter.ts
+++ libs/shared/src/filters/all-exeptions.filter.ts
@@ -12,7 +12,7 @@ export class AllExeptionsFilter<T> implements ExceptionFilter {
 
     const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    if (['graphql'].includes(host.getType())) {
+    if (['graphql', 'rmq'].includes(host.getType())) {
       throw new HttpException(this._response(status, request, exception), status);
     }
 
